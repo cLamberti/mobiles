3f7b2c55c164ebb48245344b19089da488c03374
cLamberti <150824061+cLamberti@users.noreply.github.com>
2026-04-25 11:07:48 -0600

    commit

--- POO_Examen_Java/Carro.java
+++ POO_Examen_Java/Carro.java
@@ -1,3 +1,6 @@
+// Clase abstracta base — define el contrato general para todos los carros
 public abstract class Carro {
+
+    // Método abstracto que obliga a las subclases a implementar su propio comportamiento
     public abstract void tipoCarroOffRoad();
 }
